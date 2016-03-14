31da88ebae7bb48c961254f87a84816edac6a71b
Lacey Vu <laceylin2010@gmail.com>
2016-03-14 13:48:58 -0700

    finished location for 3 places

--- LocationReminders/LocationReminders/ViewController.m
+++ LocationReminders/LocationReminders/ViewController.m
@@ -7,21 +7,76 @@
 //
 
 #import "ViewController.h"
+@import MapKit;
+@import CoreLocation;
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (strong, nonatomic) CLLocationManager* locationManager;
+
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self setupMapView];
+
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
 }
+
+-(void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    [self requestPermissions];
+}
+
+-(void)setupMapView
+{
+    self.mapView.layer.cornerRadius = 20;
+    self.mapView.showsUserLocation = YES;
+}
+
+-(void)requestPermissions
+{
+    [self setLocationManager: [[CLLocationManager alloc]init]];
+    [self.locationManager requestWhenInUseAuthorization];
+    
+}
+- (IBAction)spaceNeedleButtonSelected:(UIButton *)sender
+{
+
+    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(47.6204 , -122.349348);
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(coordinate, 500, 500);
+    [_mapView setRegion:region animated:TRUE];
+    
+
+}
+
+- (IBAction)statueOfLibertyButtonSelected:(UIButton *)sender
+{
+
+    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(40.689247, -74.044502);
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(coordinate, 50, 500);
+    [_mapView setRegion:region animated: TRUE];
+}
+
+
+- (IBAction)grandCanyonButtonSelected:(UIButton *)sender
+{
+    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(36.056595, -112.125092);
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(coordinate, 500, 500);
+    [_mapView setRegion:region animated:TRUE];
+
+}
+
 
 @end
